35a4eaf091c7404fc8866c8d7974c776d44130bf
astratow <davno_pora@mail.com>
2022-03-14 05:24:30 +0000

    class_properties.php added

--- class_inheritance.php
+++ class_inheritance.php
@@ -4,17 +4,19 @@ class Cars{
 
 	var $wheels = 4;
 	function greeting(){
-		echo "Hello";
+		echo "Hello" . '<br>';
 	}
 }
 
 $bmw = new Cars();
 
-class Trucks extends Cars{
+class Trucks extends Cars{ //extends keyword decides of inheritance
 
 }
 
 $tacoma = new Trucks();
+echo $tacoma->wheels;
+$bmw->greeting();
 echo 'inheritance test';
 
 ?>
