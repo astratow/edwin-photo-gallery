2cf94dfba70387ccbdff26374a2ba90807abcd08
astratow <davno_pora@mail.com>
2022-03-21 06:02:01 +0000

    chapters 21-30 redone in order to find error. Error occurs on change to class system

--- 29-Creating_the_Database_Class/admin/includes/database.php
+++ 29-Creating_the_Database_Class/admin/includes/database.php
@@ -13,7 +13,7 @@ class Database {
 	public function open_db_connection(){
 
 		$this->connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
-
+// this->connection = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 
 		if (mysqli_connect_errno()){
 			die("Database connection failed." . mysqli_error());
